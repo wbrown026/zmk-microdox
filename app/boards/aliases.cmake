b690ab18332bdef9d9f5430bb068f42010d5c008
wb-gd <will.brown.gd@gmail.com>
2026-05-16 02:49:27 -0400

    Compile

--- app/boards/aliases.cmake
+++
@@ -1,2 +0,0 @@
-# defines board aliases for shorter names (or for renaming boards)
-set(mikoto_520_BOARD_ALIAS "mikoto")
--- app/include/dt-bindings/zmk/behaviors.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 The ZMK Contributors
- *
- * SPDX-License-Identifier: MIT
- */
-
-#define ZMK_BEHAVIOR_OMIT(_name)                                                                   \
-    !(defined(ZMK_BEHAVIORS_KEEP_##_name) ||                                                       \
-      (defined(ZMK_BEHAVIORS_KEEP_ALL) && !defined(ZMK_BEHAVIORS_OMIT_##_name)))
